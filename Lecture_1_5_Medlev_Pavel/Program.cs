5b9661c960522de9344ad5da6f0099b5c3aa3c5c
medpavol <medpavol@gmail.com>
2018-02-04 11:14:58 +0300

    Add project files.

---
+++ Lecture_1_5_Medlev_Pavel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_1_5_Medlev_Pavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
