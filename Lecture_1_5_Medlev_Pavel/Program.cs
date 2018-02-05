d0b8139534cef39dce46e26b63a9af6bc53655c2
medpavol <medpavol@gmail.com>
2018-02-05 12:24:34 +0300

    v 0.9.9

--- Lecture_1_5_Medlev_Pavel/Program.cs
+++ Lecture_1_5_Medlev_Pavel/Program.cs
@@ -14,39 +14,15 @@ namespace Lecture_1_5_Medlev_Pavel
     {
         static void Main(string[] args)
         {
-
-            #region  v0.1
             string startPoint = "\n\n\n\t\t ";
             string textStr1 = "  Да пребудет с вами московское время ";
             string textStr2 = ". Говорят и показывают все телестанции страны. ";
             string textBox = string.Empty;
 
+            Console.ForegroundColor = ConsoleColor.Green;
+
             int lengthDisplayBox = 20;
             int delayTime = 300;
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            //string textBox = string.Format(textStr1 + DateTime.Now.ToLongTimeString() + textStr2);
-            //StringBuilder ticker = new StringBuilder(textBox.Remove(lengthDisplayBox, textBox.Length - lengthDisplayBox));
-            //
-            //int i = lengthDisplayBox;
-            //while (true)
-            //{
-            //    Console.Write($"{startPoint} {ticker}");
-            //
-            //    Thread.Sleep(delayTime);
-            //    Console.Clear();
-            //    ticker.Remove(0, 1);
-            //
-            //    ticker.Append(textBox[i]);
-            //    if (i == textBox.Length-1)
-            //    {
-            //        i = -1;
-            //        
-            //    }            
-            //    i++;
-            //}
-            #endregion
-
             int i = 0;
             int lengthLineBreak = 0;
 
@@ -54,7 +30,7 @@ namespace Lecture_1_5_Medlev_Pavel
             {
                 textBox = string.Format(textStr1 + DateTime.Now.ToLongTimeString() + textStr2);
 
-                Console.Write($" {startPoint}  {textBox.Substring(i, lengthDisplayBox - lengthLineBreak)}{textBox.Substring(0, lengthLineBreak)}  ");
+                Console.Write($" {startPoint} {textBox.Substring(i, lengthDisplayBox - lengthLineBreak)}{textBox.Substring(0, lengthLineBreak)}");
                 Thread.Sleep(delayTime);
                 Console.Clear();
 
@@ -64,9 +40,11 @@ namespace Lecture_1_5_Medlev_Pavel
                     i = 0;
                     lengthLineBreak = 0;
                 }
-
+                
                 else if ( i == textBox.Length - lengthDisplayBox + lengthLineBreak)
-                    lengthLineBreak++;               
+                    lengthLineBreak++;
+
+
             }
         }
     }
