179eea48fb81d66e70a822867b026a0c585857bd
medpavol <medpavol@gmail.com>
2018-02-05 13:50:19 +0300

    v 1.0

--- Lecture_1_5_Medlev_Pavel/Program.cs
+++ Lecture_1_5_Medlev_Pavel/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Lecture_1_5_Medlev_Pavel
 {
@@ -14,38 +10,45 @@ namespace Lecture_1_5_Medlev_Pavel
     {
         static void Main(string[] args)
         {
-            string startPoint = "\n\n\n\t\t ";
-            string textStr1 = "  Да пребудет с вами московское время ";
-            string textStr2 = ". Говорят и показывают все телестанции страны. ";
-            string textBox = string.Empty;
+            string infoText = "Количество отображаемых символов в строке = {0}. Задержка = {1} мс. /esc- выход/";
+            string startPoint = "\n\n\n\n\t\t\t ";
+            string textStr1 = "  Московское время  ";
+            string textStr2 = ".  Время как вода - течет и изменяется.. ";
 
             Console.ForegroundColor = ConsoleColor.Green;
 
-            int lengthDisplayBox = 20;
-            int delayTime = 300;
+            int lengthDisplayBox = 15;
+            int delayTime = 500;
             int i = 0;
             int lengthLineBreak = 0;
 
-            while (true)
+            do
             {
-                textBox = string.Format(textStr1 + DateTime.Now.ToLongTimeString() + textStr2);
-
-                Console.Write($" {startPoint} {textBox.Substring(i, lengthDisplayBox - lengthLineBreak)}{textBox.Substring(0, lengthLineBreak)}");
-                Thread.Sleep(delayTime);
+                Console.WriteLine(infoText, lengthDisplayBox, delayTime);
+                Thread.Sleep(delayTime * 8);
                 Console.Clear();
 
-                i++;                
-                if (i == textBox.Length)
+                while (!Console.KeyAvailable)
                 {
-                    i = 0;
-                    lengthLineBreak = 0;
+                    string textBox = textStr1 + DateTime.Now.ToLongTimeString() + textStr2;
+
+                    Console.Write($" {startPoint} {textBox.Substring(i, lengthDisplayBox - lengthLineBreak)}{textBox.Substring(0, lengthLineBreak)}");
+                    Thread.Sleep(delayTime);
+                    Console.Clear();
+
+                    i++;
+                    if (i == textBox.Length)
+                    {
+                        i = 0;
+                        lengthLineBreak = 0;
+                    }
+
+                    else if (i == textBox.Length - lengthDisplayBox + lengthLineBreak)
+                        lengthLineBreak++;
                 }
-                
-                else if ( i == textBox.Length - lengthDisplayBox + lengthLineBreak)
-                    lengthLineBreak++;
 
-
-            }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);            
         }
     }
 }
+
