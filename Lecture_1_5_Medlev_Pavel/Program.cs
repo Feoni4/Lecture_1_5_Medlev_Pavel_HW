1425ee8177d5dbcae0f9f92fdd6fd3a267f2616a
medpavol <medpavol@gmail.com>
2018-02-05 07:27:27 +0300

    v 0.1

--- Lecture_1_5_Medlev_Pavel/Program.cs
+++ Lecture_1_5_Medlev_Pavel/Program.cs
@@ -15,16 +15,15 @@ namespace Lecture_1_5_Medlev_Pavel
         static void Main(string[] args)
         {
             string startPoint = "\n\n\n\t\t ";
-            string textBox = "  Да пребудет с вами московское время. Следующее обретение силы ";
+            string textStr1 = "  Да пребудет с вами московское время ";
+            string textStr2 = ". Говорят и показывают все телестанции страны. ";
+            string textBox = string.Format(textStr1 + DateTime.Now.ToLongTimeString() + textStr2);
 
-            
-            int lengthDisplayBox = 25;
-            int delayTime = 300;
+            int lengthDisplayBox = 40;
+            int delayTime = 200;
 
             Console.ForegroundColor = ConsoleColor.Green;
             StringBuilder ticker = new StringBuilder(textBox.Remove(lengthDisplayBox, textBox.Length - lengthDisplayBox));
-
-            //var dateTime = DateTime.Now.ToString(); 
 
             int i = lengthDisplayBox;
             while (true)
@@ -36,9 +35,14 @@ namespace Lecture_1_5_Medlev_Pavel
                 ticker.Remove(0, 1);
 
                 ticker.Append(textBox[i]);
-                if (i == textBox.Length-1)   //  dateTime.Length = 19
+                if (i == textBox.Length-1)
+                {
                     i = -1;
+                    
+                }       
+                
                 i++;
+
             }       
         }
     }
